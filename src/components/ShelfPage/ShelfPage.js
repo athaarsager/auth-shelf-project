74fbd5cdfb898c09b4e8e9e8429b62bcad85beb0
Andrew Haarsager <athaarsager@gmail.com>
2024-02-15 09:48:31 -0600

    Created useEffect and map function

--- src/components/ShelfPage/ShelfPage.js
+++ src/components/ShelfPage/ShelfPage.js
@@ -1,10 +1,31 @@
 import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { useEffect, useState } from 'react';
 
 function ShelfPage() {
+  const dispatch = useDispatch();
+  const items = useSelector(store => store.items);
+
+
+  useEffect(() => {
+    dispatch({type: "FETCH_ITEMS"});
+  }, []);
+  
+
   return (
     <div className="container">
       <h2>Shelf</h2>
       <p>All of the available items can be seen here.</p>
+      <ul>
+        {items.map(item => (
+          <div key={item.id} class="shelf-item">
+            <img src={item.url} alt={item.description} />
+            <p>{item.description}</p>
+          </div>
+        ))}
+      </ul>
+
+
     </div>
   );
 }
